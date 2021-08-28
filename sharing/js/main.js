a7e7c1f7d13c285cae39fc7da0fffe95a7590cdc
NeProger-1 <77925384+NeProger-1@users.noreply.github.com>
2021-08-28 22:07:39 +0300

    Add files via upload

---
+++ sharing/js/main.js
@@ -0,0 +1,45 @@
+$(function(){
+  $('.top-slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    fade: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: false
+        }
+      },
+    ]
+  });
+  $('.reviews-slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1141,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 846,
+        settings: {
+          slidesToShow  : 2,
+        }
+      },
+      {
+        breakpoint: 585,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  })
+  $('.menu-btn').on('click', function(){
+    $('.menu-list').toggleClass('menu-list-active')
+  })
+});
